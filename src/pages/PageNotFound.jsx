e85a0641078e30e4aa36aab3c7b45ea9137c0047
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-02 09:15:07 +0200

    Add prebuild files (pages with basic styles, data, images)

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from '../components/PageNav'
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      Page not found...
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
