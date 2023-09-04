b4b9ff0586028726816e60c24b8804b9f40a32cd
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-04 13:45:07 +0200

    Add custom hook to get user current geo position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,12 +11,18 @@ import {
 } from 'react-leaflet'
 import { useEffect, useState } from 'react'
 import { useCities } from '../contexts/CitiesContext'
+import { useGeolocation } from '../hooks/useGeolocation'
+import Button from './Button'
 
 export default function Map() {
   const { cities } = useCities()
-
   const [mapPosition, setMapPosition] = useState([40, 0])
   const [searchParams] = useSearchParams()
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation()
 
   const mapLat = searchParams.get('lat')
   const mapLng = searchParams.get('lng')
@@ -28,8 +34,21 @@ export default function Map() {
     [mapLat, mapLng]
   )
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+    },
+    [geolocationPosition]
+  )
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
