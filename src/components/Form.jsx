32fbc325a496a42b1e1370d5e01bf89065285c34
MickRubashkin <mikhail.rubashkin@gmail.com>
2023-09-04 12:45:15 +0200

    Add leaflet map to the Map component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 
 import styles from './Form.module.css'
 import Button from './Button'
-import { useNavigate } from 'react-router-dom'
 import BackButton from './BackButton'
 
 export function convertToEmoji(countryCode) {
@@ -16,7 +15,6 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate()
   const [cityName, setCityName] = useState('')
   const [country, setCountry] = useState('')
   const [date, setDate] = useState(new Date())
